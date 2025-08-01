ae897f40d88dca0c4f9c34bb86cdcef354e516c7
Melvin <melvin.lin.work@gmail.com>
2025-08-01 14:04:13 +0700

    chore: install react-plotly.js for skill section

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,81 +1,76 @@
-'use client';
+"use client";
 
 import FloatingEdge from "@/components/react-flow/edges/FloatingEdge";
-import {
-    Background,
-    Edge,
-    Node,
-    ReactFlow
-} from "@xyflow/react";
+import { Background, Edge, Node, ReactFlow } from "@xyflow/react";
 import "@xyflow/react/dist/style.css";
 import type { ReactElement } from "react";
 import { FaLinkedin } from "react-icons/fa6";
 
-export default function Home(): ReactElement {
-  const nodes: Node[] = [
+const nodes: Node[] = [
     {
-      id: "n1",
-      position: { x: 0, y: 0 },
-      data: { label: "Node 1" },
+        id: "n1",
+        position: { x: 0, y: 0 },
+        data: { label: "Node 1" },
     },
     {
-      id: "n2",
-      position: { x: 100, y: 100 },
-      data: { label: "Node 2" },
+        id: "n2",
+        position: { x: 100, y: 100 },
+        data: { label: "Node 2" },
     },
     {
-      id: "n3",
-      position: { x: 200, y: 0 },
-      data: { label: "Node 3" },
+        id: "n3",
+        position: { x: 200, y: 0 },
+        data: { label: "Node 3" },
     },
-  ];
+];
 
-  const edges: Edge[] = [
+const edges: Edge[] = [
     {
-      id: "n1-n2",
-      source: "n1",
-      target: "n2",
-      type: "floating",
+        id: "n1-n2",
+        source: "n1",
+        target: "n2",
+        type: "floating",
     },
     {
-      id: "n2-n3",
-      source: "n2",
-      target: "n3",
-      type: "floating",
+        id: "n2-n3",
+        source: "n2",
+        target: "n3",
+        type: "floating",
     },
-  ];
+];
 
-  const edgeTypes = {
+const edgeTypes = {
     floating: FloatingEdge,
-  };
+};
 
-  return (
-    <>
-      <section className="container">
-        <div className="py-20">
-          <h1 className="text-6xl">Melvin Lin</h1>
-          <h4 className="mt-4 mb-8">Problem Solver...</h4>
-          <ul className="flex space-x-2">
-            {[...Array(5)].map((_, index) => (
-              <li key={index}>
-                <a href="" className="inline-block p-4 rounded-full bg-gray-200">
-                  <FaLinkedin />
-                </a>
-              </li>
-            ))}
-          </ul>
-          <button type="button" className="py-2 px-4 rounded-full bg-gray-200 mt-4">
-            Discover More
-          </button>
-        </div>
-      </section>
-      <section>
-        <div className="floating-edges h-100 border">
-          <ReactFlow nodes={nodes} edges={edges} edgeTypes={edgeTypes} fitView>
-            <Background />
-          </ReactFlow>
-        </div>
-      </section>
-    </>
-  );
+export default function HomePage(): ReactElement {
+    return (
+        <>
+            <section className="container">
+                <div className="py-20">
+                    <h1 className="text-6xl">Melvin Lin</h1>
+                    <h4 className="mt-4 mb-8">Problem Solver...</h4>
+                    <ul className="flex space-x-2">
+                        {[...Array(5)].map((_, index) => (
+                            <li key={index}>
+                                <a href="" className="inline-block p-4 rounded-full bg-gray-200">
+                                    <FaLinkedin />
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                    <button type="button" className="py-2 px-4 rounded-full bg-gray-200 mt-4">
+                        Discover More
+                    </button>
+                </div>
+            </section>
+            <section>
+                <div className="floating-edges h-100 border">
+                    <ReactFlow nodes={nodes} edges={edges} edgeTypes={edgeTypes} fitView>
+                        <Background />
+                    </ReactFlow>
+                </div>
+            </section>
+        </>
+    );
 }
