603132d330e807ae6a65db867d0a85fcaa240711
Melvin <melvin.lin.work@gmail.com>
2025-08-15 17:32:16 +0700

    fix(home/style): prevent content from being cut off in work experience timeline

--- src/app/home/components/WorkExperienceTimelineItem.tsx
+++ src/app/home/components/WorkExperienceTimelineItem.tsx
@@ -36,7 +36,7 @@ export default function WorkExperienceTimelineItem({ workExperience, isActive }:
             <div className={`relative mt-3 ${showDetail ? "pb-7" : ""}`}>
                 {showDetail && (
                     <>
-                        <input type="checkbox" id={collapsableId} className="checked:[&+label_.icon]:rotate-180 checked:[&~ul]:max-h-[300px] checked:[&~ul]:after:h-0" hidden />
+                        <input type="checkbox" id={collapsableId} className="checked:[&+label_.icon]:rotate-180 checked:[&~ul]:max-h-[600px] sm:checked:[&~ul]:max-h-[400px] lg:checked:[&~ul]:max-h-[300px] checked:[&~ul]:after:h-0" hidden />
                         <label
                             htmlFor={collapsableId}
                             className={`absolute bottom-0 right-0 z-10 flex items-end space-x-1 cursor-pointer transition-all ${isActive ? "hover:text-white/60" : "text-cyan-500 hover:text-cyan-700"}`}
@@ -47,7 +47,7 @@ export default function WorkExperienceTimelineItem({ workExperience, isActive }:
                     </>
                 )}
                 <ul
-                    className={`pl-5 list-disc space-y-0.5 max-h-[130px] overflow-hidden transition-all after:transition-all after:absolute after:bottom-0 after:left-0 after:w-full ${
+                    className={`pl-5 list-disc space-y-0.5 ${showDetail ? "max-h-[130px]" : ""} overflow-hidden transition-all after:transition-all after:absolute after:bottom-0 after:left-0 after:w-full ${
                         showDetail ? "after:h-28" : "after:h-0"
                     } after:bg-gradient-to-t ${isActive ? "after:from-[#06b6d4]" : "after:from-white"} after:to-transparent`}
                 >
