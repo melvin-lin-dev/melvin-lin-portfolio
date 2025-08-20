efd22d86deb8f6377cae305cf0d4f17a206928ef
Melvin <melvin.lin.work@gmail.com>
2025-08-20 15:40:11 +0700

    feat: add text-3d component

---
+++ src/common/components/text-3d/Text3D.tsx
@@ -0,0 +1,21 @@
+import Animate from "@/components/framer-motion/Animate";
+import { fadeUp } from "@/lib/utils/framer-motion/motions";
+import { type motion, type Variants } from "framer-motion";
+import type { ReactElement, ReactNode } from "react";
+import styles from "./text-3d.module.scss";
+import clsx from "clsx";
+
+type Text3DProps = {
+    tag: keyof typeof motion;
+    children: ReactNode;
+    variants?: Variants;
+    className?: string;
+};
+
+export default function Text3D({ tag, children, variants = fadeUp, className }: Text3DProps): ReactElement {
+    return (
+        <Animate tag={tag} variants={variants} className={clsx(styles.text3D, className)}>
+            {children}
+        </Animate>
+    );
+}
